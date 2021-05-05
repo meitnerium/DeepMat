6edc1dd5e10dcd96a04872a3beb23696a47b8096
Francois Dion <Francois.Dion@drdc-rddc.gc.ca>
2021-05-05 09:37:26 -0500

    new file:   test.py

---
+++ tutorial_chemml/test.py
@@ -0,0 +1,9 @@
+from chemml.chem import Molecule
+caffeine_smiles = 'CN1C=NC2=C1C(=O)N(C(=O)N2C)C'
+caffeine_smarts = '[#6]-[#7]1:[#6]:[#7]:[#6]2:[#6]:1:[#6](=[#8]):[#7](:[#6](=[#8]):[#7]:2-[#6])-[#6]'
+caffeine_inchi = 'InChI=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3'
+mol = Molecule(caffeine_smiles, input_type='smiles')
+mol.visualize()
+mol.hydrogens('add')
+mol.visualize()
+
